60c15967b83b80eb3b1132b486f03da88e248120
mostafa-dci <mostafa.othman@digitalcareerinstitute.org>
2019-08-22 21:41:34 +0200

    CSS

---
+++ responsivePage/script.js
@@ -0,0 +1,36 @@
+// alert(2+6*9+5-9+77);
+/*console.log("sdghsdff")
+console.log("sdghsdff")
+console.log("sdghsdff")
+console.log("sdghsdff")*/
+console.log(555*999+6-9+454)
+console.log(x)// i dont know what x  is until now
+ var x=9.5;
+ z=10;// not allowed
+var y=5;
+var binary=0b10101101// how to define anumber in binary
+var hexNumber= 0xA// to define hex Numbers
+console.log("0xA : "+hexNumber);
+// console.log(here you can put any thing to display)
+console.log(binary+hexNumber);
+var str="10102564635158325684354836584368436543683684346384585441464";
+y=x;
+console.log(y);
+var h04mn=1// no numbers first when u define a variable
+console.log(h04mn);
+var CAP=5
+
+console.log(CAP);
+var CAp=10// javascript is sesitive to upper-lowercase
+console.log(CAp);
+var s="Sehr geehrte Damen und Herren            ";
+console.log(CAp);
+var firstNumber= 5;
+var seconrNumber=7;
+var thirdNumber=10;
+var avr=(firstNumber+seconrNumber+thirdNumber)/3
+console.log("The avrage is : "+avr)
+var firstNumber=5;//not allowed cus already defiend
+firstNumber=10;// allowed that is assigning a new value
+
+
